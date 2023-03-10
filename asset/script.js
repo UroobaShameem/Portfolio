5c82ad9f6a0eb58606ba5abf3ad46de527b01851
UroobaShameem <uroobashameem12@gmail.com>
2023-03-11 02:10:47 +0500

    commit

--- asset/script.js
+++ asset/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
- 
-    $('.navbar').makeNavbar();
-
-    $('#hoverline').hoverline();
-
-
-
+$(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $("button.navbar-toggler").click();
+        }
+    });
 });
